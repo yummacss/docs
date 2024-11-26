a9f378328bb1edcfed34c5546e8904ccb7182481
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-11-26 19:43:44 +0000

    docs: update `maxHeadingLevel` to 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "Yumma CSS",
+      tableOfContents: { maxHeadingLevel: 5 },
       expressiveCode: {
         themes: [ariakeTheme],
         styleOverrides: {
