057f556df953e29d1c9479a031c45baafdcc47ea
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-12-26 11:25:31 +0000

    docs: update code examples

--- src/constants/landing-cards.ts
+++ src/constants/landing-cards.ts
@@ -5,27 +5,7 @@ export const abbreviatedSyntax: string = `
 `;
 
 export const tinyAndLightweight: string = `
-.bg-cyan {
-    background-color: #05a4bf
-}
-
-.tc-white {
-    color: #fff
-}
-
-.rad-2 {
-    border-radius: .5rem
-}
-
-.px-5 {
-    padding-left: 1.25rem;
-    padding-right: 1.25rem
-}
-
-.py-2 {
-    padding-bottom: .5rem;
-    padding-top: .5rem
-}
+npx yummacss build
 `;
 
 export const responsiveByDesignSmall: string = `
@@ -60,7 +40,7 @@ export const responsiveByDesignXXLarge: string = `
 
 export const uiComponents: string = `
 import React from "react";
-import { Button, Footer. Navbar } from "yumma-ui";
+import { Button, Footer, Navbar } from "yumma-ui";
 `;
 
 export const serverComponents: string = `
