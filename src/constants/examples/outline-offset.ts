da6f9ac9e60205dc678fc03fbf248d1062f13c2e
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-01-02 21:52:06 +0000

    refactor: remove unused outline-offset example and update related documentation

--- src/constants/examples/outline-offset.ts
+++
@@ -1,6 +0,0 @@
-export const base: string = `
-<div class="d-g g-16 gtc-2">
-    <button class="b-1 bc-l-silver-6 bg-white fw-600 oc-indigo oo-2 os-s px-5 py-1 rad-1">Subscribe</button>
-    <button class="b-1 bc-l-silver-6 bg-white fw-600 oc-indigo oo-4 os-s px-5 py-1 rad-1">Subscribe</button>
-</div>
-`;
