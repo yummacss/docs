2fb857ff7cfc07cb13ee8ba6042f547d40437ba2
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-01-03 02:26:24 +0000

    refactor: remove unused imports from documentation files

--- src/constants/examples/box-shadow.ts
+++
@@ -1,8 +0,0 @@
-export const base: string = `
-<div class="d-g g-16 gtc-4">
-    <div class="ai-c bg-white bs-xs d-f dim-16 fw-700 jc-c rad-2 tc-indigo">xs</div>
-    <div class="ai-c bg-white bs-sm d-f dim-16 fw-700 jc-c rad-2 tc-indigo">sm</div>
-    <div class="ai-c bg-white bs-md d-f dim-16 fw-700 jc-c rad-2 tc-indigo">md</div>
-    <div class="ai-c bg-white bs-lg d-f dim-16 fw-700 jc-c rad-2 tc-indigo">lg</div>
-</div>
-`;
--- src/constants/examples/opacity.ts
+++
@@ -1,8 +0,0 @@
-export const base: string = `
-<div class="d-g g-16 gtc-4">
-    <div class="o-20 ai-c bg-indigo d-f dim-16 fw-700 jc-c rad-2 tc-white">20</div>
-    <div class="o-50 ai-c bg-indigo d-f dim-16 fw-700 jc-c rad-2 tc-white">50</div>
-    <div class="o-70 ai-c bg-indigo d-f dim-16 fw-700 jc-c rad-2 tc-white">70</div>
-    <div class="o-100 ai-c bg-indigo d-f dim-16 fw-700 jc-c rad-2 tc-white">100</div>
-</div>
-`;
