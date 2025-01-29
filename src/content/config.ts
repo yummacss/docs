f51bf3aa39dda3ae86900fb584cfdae1aa9f36f5
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-01-29 22:24:15 +0000

    feat: add docs loader to content configuration

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,10 @@
 import { defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
+import { docsLoader } from "@astrojs/starlight/loaders";
 import { blogSchema } from "starlight-blog/schema";
 
 export const collections = {
+  loader: docsLoader(),
   docs: defineCollection({
     schema: docsSchema({
       extend: (context) => blogSchema(context),
