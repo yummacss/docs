66c28097a94e21a2823f16a97858b540726c39f9
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-03 18:30:23 +0000

    docs: update utilities to match with v2.1

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
-import { defineCollection } from "astro:content";
-import { docsSchema } from "@astrojs/starlight/schema";
-import { docsLoader } from "@astrojs/starlight/loaders";
 import { blogSchema } from "starlight-blog/schema";
+import { defineCollection } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
   loader: docsLoader(),
