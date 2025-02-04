69ac38ba30b5c7ed2e2c174fdf834affa1c55039
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-04 14:41:01 +0000

    ref: fix inaccessible blog pages due to bad config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,8 +4,8 @@ import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-  loader: docsLoader(),
   docs: defineCollection({
+    loader: docsLoader(),
     schema: docsSchema({
       extend: (context) => blogSchema(context),
     }),
