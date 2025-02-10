77f248b2cb292e4b41ec6c56c9216aa9b8f4900a
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-02-10 18:13:33 +0000

    deps: add starlightHeadingBadges to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import starlight from "@astrojs/starlight";
 import starlightBlog from "starlight-blog";
 import starlightCoolerCredit from "starlight-cooler-credit";
 import starlightLinksValidator from "starlight-links-validator";
+import starlightHeadingBadges from "starlight-heading-badges";
 import vercel from "@astrojs/vercel";
 
 const theme = fs.readFileSync(new URL(`./theme.jsonc`, import.meta.url), "utf-8");
@@ -46,6 +47,7 @@ export default defineConfig({
         replacesTitle: true,
       },
       plugins: [
+        starlightHeadingBadges(),
         starlightLinksValidator(),
         starlightCoolerCredit({
           credit: {
