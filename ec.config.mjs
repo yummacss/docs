41b9f24e2c996a0643568b4a484b8dc95dff3ffa
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-06-15 00:57:19 +0100

    chore: improve configuration, update package dependencies and add twoslash plugin

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,9 +1,16 @@
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import ecTwoSlash from "expressive-code-twoslash";
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
-  plugins: [pluginCollapsibleSections()],
-  defaultProps: {
-    collapseStyle: "collapsible-start",
-  },
+  plugins: [pluginCollapsibleSections(), ecTwoSlash()],
+  defaultProps: { collapseStyle: "collapsible-start" },
 };
+
+ecTwoSlash({
+  explicitTrigger: true,
+  includeJsDoc: true,
+  allowNonStandardJsDocTags: false,
+  languages: ["ts", "tsx"],
+  twoslashOptions: {},
+});
