5159ab448f29161803c6d54fd5298bf836ff3888
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-06-17 18:42:44 +0100

    chore: remove 'expressive-code-twoslash' dependency and update configuration to reflect changes in plugins

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,16 +1,7 @@
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
-import ecTwoSlash from "expressive-code-twoslash";
 
 /** @type {import('@astrojs/starlight/expressive-code').StarlightExpressiveCodeOptions} */
 export default {
-  plugins: [pluginCollapsibleSections(), ecTwoSlash()],
+  plugins: [pluginCollapsibleSections()],
   defaultProps: { collapseStyle: "collapsible-start" },
 };
-
-ecTwoSlash({
-  explicitTrigger: true,
-  includeJsDoc: true,
-  allowNonStandardJsDocTags: false,
-  languages: ["ts", "tsx"],
-  twoslashOptions: {},
-});
