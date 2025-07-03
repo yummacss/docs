72f57331ebc3d52dc5ceb2b853a621e747edc673
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-07-04 00:14:32 +0100

    feat: add Solid.js integration and client:load support to the Class component

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,113 +7,111 @@ import starlightBlog from "starlight-blog";
 import starlightLinksValidator from "starlight-links-validator";
 import starlightSidebarTopics from "starlight-sidebar-topics";
 
+import solidJs from "@astrojs/solid-js";
+
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Yumma CSS",
-      favicon: "/favicon.ico",
-      logo: {
-        alt: "Yumma CSS Logo",
-        dark: "/public/trademark/logo-dark.png",
-        light: "/public/trademark/logo-light.png",
-        replacesTitle: true,
-      },
+  integrations: [starlight({
+    title: "Yumma CSS",
+    favicon: "/favicon.ico",
+    logo: {
+      alt: "Yumma CSS Logo",
+      dark: "/public/trademark/logo-dark.png",
+      light: "/public/trademark/logo-light.png",
+      replacesTitle: true,
+    },
 
-      markdown: {
-        headingLinks: false,
-      },
+    markdown: {
+      headingLinks: false,
+    },
 
-      customCss: ["@fontsource-variable/outfit", "/src/styles/custom.css", "/src/styles/yumma.css"],
-      components: {
-        Banner: "./src/components/starlight/Banner.astro",
-        Hero: "./src/components/starlight/Index.astro",
-        PageTitle: "./src/components/starlight/PageTitle.astro",
-      },
+    customCss: ["@fontsource-variable/outfit", "/src/styles/custom.css", "/src/styles/yumma.css"],
+    components: {
+      Banner: "./src/components/starlight/Banner.astro",
+      Hero: "./src/components/starlight/Index.astro",
+      PageTitle: "./src/components/starlight/PageTitle.astro",
+    },
 
-      plugins: [
-        starlightLinksValidator(),
-        starlightSidebarTopics(config.sidebar, {
-          exclude: ["/blog", "/blog/**/*", "/brand", "/docs/guides", "/docs/guides/**/*"],
-        }),
-        starlightBlog({
-          recentPostCount: 3,
-          authors: {
-            Renildo: {
-              name: "Renildo Pereira",
-              picture: "https://avatars.githubusercontent.com/u/56491937?v=4",
-              title: "Founder / CEO",
-              url: "https://x.com/rrenildoo",
-            },
-          },
-        }),
-      ],
-
-      expressiveCode: {
-        themes: [config.midnightTheme],
-        styleOverrides: {
-          collapsibleSections: {
-            openBackgroundColorCollapsible: "hsla(231, 73%, 77%, 0.050)",
-            closedBackgroundColor: "hsla(231, 73%, 77%, 0.100)",
-          },
-          frames: {
-            shadowColor: "transparent",
-          },
-          textMarkers: {
-            delBackground: "hsla(0, 48%, 77%, 0.100)",
-            delHue: "hsl(0, 48%, 77%)",
-            insBackground: "hsla(127, 48%, 77%, 0.150)",
-            insHue: "hsl(127, 48%, 77%)",
-            markBackground: "hsla(231, 73%, 77%, 0.100)",
-            markHue: "hsl(231, 73%, 77%)",
+    plugins: [
+      starlightLinksValidator(),
+      starlightSidebarTopics(config.sidebar, {
+        exclude: ["/blog", "/blog/**/*", "/brand", "/docs/guides", "/docs/guides/**/*"],
+      }),
+      starlightBlog({
+        recentPostCount: 3,
+        authors: {
+          Renildo: {
+            name: "Renildo Pereira",
+            picture: "https://avatars.githubusercontent.com/u/56491937?v=4",
+            title: "Founder / CEO",
+            url: "https://x.com/rrenildoo",
           },
         },
+      }),
+    ],
+
+    expressiveCode: {
+      themes: [config.midnightTheme],
+      styleOverrides: {
+        collapsibleSections: {
+          openBackgroundColorCollapsible: "hsla(231, 73%, 77%, 0.050)",
+          closedBackgroundColor: "hsla(231, 73%, 77%, 0.100)",
+        },
+        frames: {
+          shadowColor: "transparent",
+        },
+        textMarkers: {
+          delBackground: "hsla(0, 48%, 77%, 0.100)",
+          delHue: "hsl(0, 48%, 77%)",
+          insBackground: "hsla(127, 48%, 77%, 0.150)",
+          insHue: "hsl(127, 48%, 77%)",
+          markBackground: "hsla(231, 73%, 77%, 0.100)",
+          markHue: "hsl(231, 73%, 77%)",
+        },
       },
+    },
 
-      editLink: {
-        baseUrl: "https://github.com/yumma-lib/yumma-css-docs/tree/release/",
+    editLink: {
+      baseUrl: "https://github.com/yumma-lib/yumma-css-docs/tree/release/",
+    },
+
+    social: [{ icon: "github", label: "GitHub", href: "https://github.com/yumma-lib/yumma-css" }],
+
+    head: [
+      {
+        tag: "link",
+        attrs: {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/favicon/apple-touch-icon.png",
+        },
       },
-
-      social: [{ icon: "github", label: "GitHub", href: "https://github.com/yumma-lib/yumma-css" }],
-
-      head: [
-        {
-          tag: "link",
-          attrs: {
-            rel: "apple-touch-icon",
-            sizes: "180x180",
-            href: "/favicon/apple-touch-icon.png",
-          },
+      {
+        tag: "meta",
+        attrs: {
+          content: "/og.png",
+          property: "og:image",
         },
-        {
-          tag: "meta",
-          attrs: {
-            content: "/og.png",
-            property: "og:image",
-          },
-        },
-      ],
-    }),
-    config.AutoImport({
-      imports: [
-        "/src/components/Class.astro",
-        "/src/components/HoverModifier.astro",
-        "/src/components/legacy/LegacyClass.astro",
-        "/src/components/legacy/LegacyColor.astro",
-        "/src/components/legacy/LegacyPalette.astro",
-        "/src/components/MediaModifier.astro",
-        "/src/components/Note.astro",
-        "/src/components/Palette.astro",
-        {
-          "@astrojs/starlight/components": ["Aside", "Card", "CardGrid", "Code", "FileTree", "Icon", "LinkButton", "LinkCard", "Steps", "TabItem", "Tabs"],
-          "starlight-package-managers": ["PackageManagers"],
-          "starlight-showcases": ["ShowcaseImage", "ShowcaseText", "ShowcaseYouTube"],
-        },
-      ],
-    }),
-    config.liveCode({
-      layout: "/src/layouts/default.astro",
-    }),
-  ],
+      },
+    ],
+  }), config.AutoImport({
+    imports: [
+      "/src/components/HoverModifier.astro",
+      "/src/components/legacy/LegacyClass.astro",
+      "/src/components/legacy/LegacyColor.astro",
+      "/src/components/legacy/LegacyPalette.astro",
+      "/src/components/MediaModifier.astro",
+      "/src/components/Note.astro",
+      "/src/components/Palette.astro",
+      "src/components/Class.tsx",
+      {
+        "@astrojs/starlight/components": ["Aside", "Card", "CardGrid", "Code", "FileTree", "Icon", "LinkButton", "LinkCard", "Steps", "TabItem", "Tabs"],
+        "starlight-package-managers": ["PackageManagers"],
+        "starlight-showcases": ["ShowcaseImage", "ShowcaseText", "ShowcaseYouTube"],
+      },
+    ],
+  }), config.liveCode({
+    layout: "/src/layouts/default.astro",
+  }), solidJs()],
 
   redirects: {
     // blog related
