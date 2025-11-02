2f85df13b9bdfd8d1c1ee471e076f75b2478f8f7
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-11-02 00:31:28 +0000

    docs: update api-reference docs

--- src/utils/sidebar.ts
+++ src/utils/sidebar.ts
@@ -70,6 +70,10 @@ export const sidebarConfig: SidebarConfig = [
     ],
   },
   {
+    title: "API Reference",
+    items: [{ title: "API Reference", slug: "api-reference" }],
+  },
+  {
     title: "Background",
     items: [
       { title: "Background Attachment", slug: "background-attachment" },
