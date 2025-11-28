2f0cb29117e1531daaed5d0fab9e61dee94c743d
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-11-28 13:59:17 +0000

    perform lint

--- src/utils/ui-sidebar.ts
+++ src/utils/ui-sidebar.ts
@@ -3,7 +3,8 @@ export interface UISidebarConfigItemBase {
   slug?: string;
 }
 
-export interface UISidebarConfigItemWithChildren extends UISidebarConfigItemBase {
+export interface UISidebarConfigItemWithChildren
+  extends UISidebarConfigItemBase {
   children: UISidebarConfigSimpleItem[];
 }
 
