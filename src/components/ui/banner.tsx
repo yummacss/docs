b1ca0306e2bb48e29237c7461191ba516e39aadd
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-11-30 00:30:03 +0000

    feat: add dynamic blog data fetch to the banner component

--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
+import { getAllBlogPosts } from "@/utils/blog";
 
-export default function Banner() {
+export default async function Banner() {
+  const posts = await getAllBlogPosts();
+  const latestPost = posts[0];
+
+  if (!latestPost) return null;
+
   return (
     <div className="p-r o-h mb-8">
       <div className="rainbow p-a i-0" />
       <div className="p-r d-f ai-c jc-c py-3 px-4 fs-sm fw-500 tc-white bf-b-sm">
-        <Link href="/blog/yummacss-3.5.0">Yumma CSS 3.5 is live! 🎉</Link>
+        <Link href={`/blog/${latestPost.slug}`}>
+          {latestPost.title} <span className="ml-2">🎉</span>
+        </Link>
       </div>
     </div>
   );
