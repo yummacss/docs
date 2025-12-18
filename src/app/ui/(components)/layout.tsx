bbc139ca8f26e4d9c244bdb8747a6df76a05d586
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-12-18 22:14:11 +0000

    refactor: remove unnecessary padding classes from layout components for cleaner design

--- src/app/ui/(components)/layout.tsx
+++ src/app/ui/(components)/layout.tsx
@@ -9,7 +9,7 @@ export default function UILayout({ children }: { children: React.ReactNode }) {
       <Navbar />
       <MobileBreadcrumb />
 
-      <main className="~sm-xxl mx-auto px-6 pt-26 lg:pt-14">
+      <main className="~sm-xxl mx-auto px-6">
         <div className="d-g gtc-1 lg:gtc-12 g-8">
           <UISidebar />
 
