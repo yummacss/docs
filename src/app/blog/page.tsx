810a01e4006c1ba7098bf91a2208694d654da4c0
Renildo Pereira <56491937+rrenildopereiraa@users.noreply.github.com>
2025-12-19 01:58:23 +0000

    style: decrease vertical padding on smaller screens

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -14,7 +14,7 @@ export default async function BlogPage() {
   const years = Array.from(postsByYear.keys()).sort((a, b) => b - a);
 
   return (
-    <div className="py-16">
+    <div className="py-10 sm:py-16">
       <div className="mx-auto my-16" style={{ maxWidth: "64rem" }}>
         <h1 className="fs-5xl fw-400 mt-6 tc-white">Blog articles</h1>
         <p className="fs-lg tc-white/70" style={{ maxWidth: "48rem" }}>
