32574a590946340bc0b64ad2e42264d4ad9aa5e6
Renildo Pereira <rrenildopereiraa@gmail.com>
2025-12-20 18:26:12 +0000

    refactor: update initial Yumma UI documentation pages and sidebar navigation

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -1,9 +1,9 @@
-import { BookIcon } from "@phosphor-icons/react/dist/ssr";
+import Silk from "@/components/silk";
+import Navbar from "@/components/ui/navbar";
+import { BookIcon, SealCheckIcon } from "@phosphor-icons/react/dist/ssr";
 import type { Metadata } from "next";
 import Link from "next/link";
 import { SiHtml5, SiNextdotjs, SiReact } from "react-icons/si";
-import Silk from "@/components/silk";
-import Navbar from "@/components/ui/navbar";
 
 export const metadata: Metadata = {
   title: {
@@ -22,14 +22,24 @@ export default function UIPage() {
       </div>
 
       <div
-        className="~sm-xxl w-full mx-auto px-6 d-f ai-c p-r zi-1"
+        className="~sm-xxl w-full mx-auto px-6 d-f ai-fe pb-12 p-r zi-1"
         style={{
           minHeight: "calc(100vh - 120px)",
         }}
       >
         <div className="w-full" style={{ maxWidth: "72rem" }}>
-          <div className="d-g lg:gtc-2 g-12 ai-c">
+          <div className="d-g lg:gtc-2 g-12 ai-fe">
             <div>
+              <Link href="https://www.w3.org/WAI/ARIA/apg/patterns/" target="_blank" className="tc-white">
+                <div className="d-if ai-c g-2 px-4 py-2 bf-b-md b-1 bg-white/5 bc-white/10 rad-9 mb-8">
+                  <span className="w-3 h-3 rad-9">
+                    <SealCheckIcon className="w-3 h-3" weight="fill" />
+                  </span>
+                  <span className="fs-xs fw-500 ls-4">
+                    We now use APG patterns!
+                  </span>
+                </div>
+              </Link>
               <h1 className="fs-5xl md:fs-6xl fw-400 mb-6 lh-1">
                 Build clean, production-ready interfaces faster.
               </h1>
@@ -43,7 +53,7 @@ export default function UIPage() {
                   href="/ui/introduction"
                   className="bg-white px-6 py-3 tc-black fs-md fw-600 rad-9"
                 >
-                  Browse now
+                  Explore UI
                 </Link>
                 <Link
                   href="/"
@@ -54,7 +64,7 @@ export default function UIPage() {
                 </Link>
               </div>
 
-              <div className="d-f ai-c g-3">
+              <div className="d-f fw-w g-3 bt-1 bc-white/10 pt-6">
                 <span className="fs-sm tc-white/60">Works with:</span>
                 <div className="d-f ai-c g-4">
                   <div className="d-f ai-c g-2 tc-white/80">
