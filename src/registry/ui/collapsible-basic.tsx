eb18352a1567beb52f5290e138473743eb57fdaa
Renildo Pereira <rrenildopereiraa@gmail.com>
2026-01-26 18:47:48 +0000

    docs (ui): update Collapsible component example

--- src/registry/ui/collapsible-basic.tsx
+++ src/registry/ui/collapsible-basic.tsx
@@ -5,13 +5,13 @@ import { CaretRightIcon } from "@phosphor-icons/react";
 
 export default function ExampleCollapsible() {
   return (
-    <Collapsible.Root className="d-f fd-c jc-c w-56 min-h-36 c-slate">
-      <Collapsible.Trigger className="d-f ai-c g-2 br-1 bg-silver-1 px-2 py-1 fs-sm fw-500 c-slate h:bg-silver-2 fv:os-s fv:ow-2 fv:oc-blue-8 collapsible-trigger">
+    <Collapsible.Root className="d-f fd-c jc-c w-56 min-h-36 c-slate-12">
+      <Collapsible.Trigger className="d-f ai-c g-2 br-1 bg-silver-1 px-2 py-1 fs-sm fw-500 c-slate-12 h:bg-silver-2 fv:os-s fv:ow-2 fv:oc-blue-8 collapsible-trigger">
         <CaretRightIcon size={12} weight="bold" className="collapsible-icon" />
         Recovery keys
       </Collapsible.Trigger>
       <Collapsible.Panel className="d-f fd-c jc-fe o-h fs-sm collapsible-panel">
-        <div className="d-f fd-c g-2 br-1 bg-silver-1 mt-1 py-2 pl-7 c-t">
+        <div className="d-f fd-c g-2 br-1 bg-silver-1 mt-1 py-2 pl-7">
           <div>alien-bean-pasta</div>
           <div>wild-irish-burrito</div>
           <div>horse-battery-staple</div>
