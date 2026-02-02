b8f81cfc24993138532c4afda3500bbf10ae1caf
Renildo Pereira <rrenildopereiraa@gmail.com>
2026-02-02 23:00:39 +0000

    refactor(ui): update Fieldset Yumma UI component with consistent styling

--- src/registry/ui/fieldset-basic.tsx
+++ src/registry/ui/fieldset-basic.tsx
@@ -4,23 +4,29 @@ import { Fieldset } from "@base-ui/react/fieldset";
 export default function ExampleFieldset() {
   return (
     <Fieldset.Root className="d-f fd-c g-4 w-full max-w-64 bw-0 m-0 p-0">
-      <Fieldset.Legend className="bbw-1 bc-silver-4 pb-3 fs-lg fw-500 c-slate-12">
-        Billing details
+      <Fieldset.Legend className="bbw-1 bc-silver-2 pb-3 fs-lg fw-600 c-slate-10">
+        Contact info
       </Fieldset.Legend>
 
-      <Field.Root className="d-f fd-c ai-fs g-1">
-        <Field.Label className="fs-sm fw-500 c-slate-12">Company</Field.Label>
+      <Field.Root className="d-f fd-c ai-fs g-2">
+        <Field.Label className="fs-sm fw-600 c-slate-10">
+          Full name <span className="c-indigo">*</span>
+        </Field.Label>
         <Field.Control
-          placeholder="Enter company name"
-          className="h-10 w-full br-1 bw-1 bc-silver-4 pl-4 fs-md c-slate-12 bg-white fv:os-s fv:ow-2 fv:oo--1 fv:oc-blue-8"
+          required
+          placeholder="John Doe"
+          className="h-10 w-full br-2 bw-1 bc-silver-3 pl-4 fs-sm c-slate-10 bg-white fv:os-s fv:ow-2 fv:oo-2 fv:oc-indigo-6"
         />
       </Field.Root>
 
-      <Field.Root className="d-f fd-c ai-fs g-1">
-        <Field.Label className="fs-sm fw-500 c-slate-12">Tax ID</Field.Label>
+      <Field.Root className="d-f fd-c ai-fs g-2">
+        <Field.Label className="fs-sm fw-600 c-slate-10">
+          Phone number
+        </Field.Label>
         <Field.Control
-          placeholder="Enter fiscal number"
-          className="h-10 w-full br-1 bw-1 bc-silver-4 pl-4 fs-md c-slate-12 bg-white fv:os-s fv:ow-2 fv:oo--1 fv:oc-blue-8"
+          type="tel"
+          placeholder="+1 (555) 000-0000"
+          className="h-10 w-full br-2 bw-1 bc-silver-3 pl-4 fs-sm c-slate-10 bg-white fv:os-s fv:ow-2 fv:oo-2 fv:oc-indigo-6"
         />
       </Field.Root>
     </Fieldset.Root>
