36e1210a9c68396511a6e6c946e8f5553e15b9c7
Renildo Pereira <rrenildopereiraa@gmail.com>
2026-02-02 23:25:10 +0000

    refactor(ui): update Meter Yumma UI component with consistent styling

--- src/registry/ui/meter-basic.tsx
+++ src/registry/ui/meter-basic.tsx
@@ -4,24 +4,16 @@ import { Meter } from "@base-ui/react/meter";
 
 export default function ExampleMeter() {
   return (
-    <Meter.Root className="d-g gtc-2 rg-2 w-48" value={24}>
-      <Meter.Label className="fs-sm fw-500 c-slate-12">
-        Storage Used
-      </Meter.Label>
-      <Meter.Value className="m-0 fs-sm c-slate-12 ta-r gcs-2" />
-      <Meter.Track className="o-h bg-silver-1 h-2 meter-track">
-        <Meter.Indicator className="d-b bg-slate-5 h-full meter-indicator" />
+    <Meter.Root className="d-f fd-c g-2 w-64" value={68}>
+      <div className="d-f jc-sb ai-c">
+        <Meter.Label className="fs-sm fw-600 c-slate-10">
+          Memory usage
+        </Meter.Label>
+        <Meter.Value className="fs-sm c-slate-8" />
+      </div>
+      <Meter.Track className="o-h bg-silver-2 h-2 br-pill">
+        <Meter.Indicator className="d-b bg-indigo h-full br-pill tp-w tdu-500 ttf-io" />
       </Meter.Track>
-
-      <style>{`
-        .meter-track {
-          grid-column: 1 / 3;
-          box-shadow: inset 0 0 0 1px #dadcdf;
-        }
-        .meter-indicator {
-          transition: width 500ms;
-        }
-      `}</style>
     </Meter.Root>
   );
 }
