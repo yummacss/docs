ec7f3b1a6ad3eac86f8dce93d58d278445e6386f
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-03-24 00:43:25 +0000

    chore: Remove web-haptics dependency.

--- src/registry/ui/context-menu-base.tsx
+++ src/registry/ui/context-menu-base.tsx
@@ -82,7 +82,7 @@ export default function ExampleContextMenu() {
                 <ContextMenu.Item
                   className={(state) =>
                     `d-f py-2 pr-8 pl-4 fs-sm us-none c-p br-sm mx-1 ${
-                      state.highlighted ? "bg-red-1" : "h:bg-red-1"
+                      state.highlighted ? "bg-red-1 c-red" : "h:bg-red-1"
                     }`
                   }
                 >
