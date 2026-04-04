e53aac13312bc9ef6cfc084cfa643c644f55f9fd
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-04 01:54:16 +0100

    clean up legacy documentation redirects

--- redirects.ts
+++ redirects.ts
@@ -4,78 +4,9 @@ const docsRedirects = [
     destination: "/docs/colors",
     permanent: true,
   },
-  ...["/docs/cli", "/docs/config"].map((source) => ({
-    source,
-    destination: "/docs/configuration",
-    permanent: true,
-  })),
-  {
-    source: "/docs/migration-guide",
-    destination: "/docs/upgrading",
-    permanent: true,
-  },
-  {
-    source: "/docs/text-color",
-    destination: "/docs/color",
-    permanent: true,
-  },
-  {
-    source: "/docs/dimensions",
-    destination: "/docs/upgrading#removed-utilities",
-    permanent: true,
-  },
-  {
-    source: "/docs/spacing",
-    destination: "/docs/upgrading#removed-utilities",
-    permanent: true,
-  },
-  {
-    source: "/resources",
-    destination: "/docs/installation",
-    permanent: true,
-  },
-  {
-    source: "/docs/utility-classes",
-    destination: "/docs/styling-elements",
-    permanent: true,
-  },
-  {
-    source: "/docs/code-editor",
-    destination: "/docs/ide-support",
-    permanent: true,
-  },
-  {
-    source: "/docs/top-right-bottom-left",
-    destination: "/docs/top",
-    permanent: true,
-  },
-];
-
-const frameworkRedirects = [
-  ...[
-    "react",
-    "nextjs",
-    "preact",
-    "vue",
-    "nuxtjs",
-    "angular",
-    "astro",
-    "qwik",
-    "solid",
-    "svelte",
-  ].map((framework) => ({
-    source: `/docs/guides/${framework}`,
-    destination: "/docs/installation",
-    permanent: true,
-  })),
 ];
 
 const blogRedirects = [
-  {
-    source: "/blog/code-editor",
-    destination: "/blog/intellisense",
-    permanent: true,
-  },
   {
     source: "/blog/v0",
     destination: "/blog/yummacss-0.1",
@@ -109,11 +40,6 @@ const blogRedirects = [
   {
     source: "/blog/yummacss-3.0",
     destination: "/blog/yummacss-3.0.0",
-    permanent: true,
-  },
-  {
-    source: "/blog/api-1.0.0",
-    destination: "/blog/yummacss-api",
     permanent: true,
   },
 ];
@@ -172,9 +98,4 @@ const uiRedirects = [
   })),
 ];
 
-export const redirects = [
-  ...docsRedirects,
-  ...frameworkRedirects,
-  ...blogRedirects,
-  ...uiRedirects,
-];
+export const redirects = [...docsRedirects, ...blogRedirects, ...uiRedirects];
