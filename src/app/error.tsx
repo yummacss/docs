65d231d6b63b8506a1bd6adb56f76d81d9cb36e9
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-08 18:19:01 +0100

    style: small changes to the error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -12,14 +12,10 @@ export default function ErrorPage({ error }: Props) {
       <div className="d-f fd-c ai-c">
         <div className="d-f ai-c mb-4">
           <h1 className="mr-5 pr-6 bc-white/20 brw-1 ff-e fs-4xl">500</h1>
-          <h2 className="m-0 fs-md fw-400">
-            Something went wrong on our side.
-          </h2>
+          {error?.message && (
+            <h2 className="m-0 fs-md fw-400">{error.message}</h2>
+          )}
         </div>
-
-        {error?.message && (
-          <p className="c-white/70 m-0 mt-2 fs-xs">{error.message}</p>
-        )}
       </div>
     </div>
   );
