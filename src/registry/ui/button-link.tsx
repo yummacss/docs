e9724bca3425f39295f2a09cb878b569775b360e
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-11 23:39:46 +0100

    small changes to the Button Link example

--- src/registry/ui/button-link.tsx
+++ src/registry/ui/button-link.tsx
@@ -2,7 +2,7 @@ import { Button } from "@base-ui/react/button";
 
 export default function ButtonLink() {
   return (
-    <Button className="px-0 py-0 bg-transparent c-indigo h:c-indigo-8 br-0 bw-0 fw-600 fs-md td-u tp-c tdu-150 ttf-io us-none c-p fv:os-s fv:ow-2 fv:oo-2 fv:oc-indigo-6">
+    <Button className="px-3 py-2 bg-transparent c-slate-10 br-md bw-0 fw-600 tuo-2 us-none c-p h:td-u fv:os-s fv:ow-2 fv:oo-2 fv:oc-indigo-6">
       Learn more
     </Button>
   );
