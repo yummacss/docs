4e961854cc311c20abdb2a2c3eb363575d45d932
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-17 00:09:58 +0100

    add us-none utility to Badge components

--- src/registry/ui/badge-pilled.tsx
+++ src/registry/ui/badge-pilled.tsx
@@ -4,7 +4,7 @@ export default function BadgePilled() {
   return (
     <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 br-pill bw-1 bs-o-xs">
       <span className="d-f ai-c jc-c w-2 h-2 bg-green br-pill" />
-      <span className="c-slate-10 fs-xs fw-500">Online</span>
+      <span className="c-slate-10 fs-xs fw-500 us-none">Online</span>
     </div>
   );
 }
--- src/registry/ui/badge-soft-pilled.tsx
+++ src/registry/ui/badge-soft-pilled.tsx
@@ -3,7 +3,7 @@
 export default function BadgeSoftPilled() {
   return (
     <div className="d-f ai-c g-1 px-2 py-1 bg-green/10 bc-green-2 br-pill bw-1">
-      <span className="c-green fs-xs fw-500">Online</span>
+      <span className="c-green fs-xs fw-500 us-none">Online</span>
     </div>
   );
 }
