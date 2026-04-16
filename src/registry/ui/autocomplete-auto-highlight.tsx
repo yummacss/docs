e8d96ee5d93ebe0650bfc1cb305cfbf8bb9146a7
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-17 00:20:37 +0100

    add bs-o-xs utility to Autocomplete components

--- src/registry/ui/autocomplete-auto-highlight.tsx
+++ src/registry/ui/autocomplete-auto-highlight.tsx
@@ -24,7 +24,7 @@ export default function AutocompleteAutoHighlight() {
         <Autocomplete.Input
           id="auto-highlight-input"
           placeholder="e.g.Rainbow"
-          className="h-10 w-64 pl-4 bg-white bc-silver-3 c-slate-12 bw-1 br-lg fs-md fv:ow-2 fv:oo--1 fv:oc-indigo-6"
+          className="h-10 w-64 pl-4 bg-white bc-silver-3 c-slate-12 bw-1 br-lg fs-md bs-o-xs fv:ow-2 fv:oo--1 fv:oc-indigo-6"
         />
       </div>
 
--- src/registry/ui/autocomplete-grouped.tsx
+++ src/registry/ui/autocomplete-grouped.tsx
@@ -22,7 +22,7 @@ export default function AutocompleteGrouped() {
         <Autocomplete.Input
           id="grouped-input"
           placeholder="e.g. general"
-          className="h-10 w-64 pl-4 bg-white bc-silver-3 c-slate-12 bw-1 br-lg fs-md fv:ow-2 fv:oo--1 fv:oc-indigo-6"
+          className="h-10 w-64 pl-4 bg-white bc-silver-3 c-slate-12 bw-1 br-lg fs-md bs-o-xs fv:ow-2 fv:oo--1 fv:oc-indigo-6"
         />
       </div>
 
