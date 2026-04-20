358b9febe2d4b844b10ff037f46b7638499f57fe
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-20 13:48:04 +0100

    style: update badge components to use indigo-6 background color and add new CSS class for indigo-6

--- src/registry/ui/badge-pilled.tsx
+++ src/registry/ui/badge-pilled.tsx
@@ -3,8 +3,8 @@
 export default function BadgePilled() {
   return (
     <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 br-pill bw-1 bs-o-xs">
-      <span className="d-f ai-c jc-c w-2 h-2 bg-green br-pill" />
+      <span className="d-f ai-c jc-c w-2 h-2 bg-indigo-6 br-pill" />
       <span className="c-slate-10 fs-xs fw-500 us-none">Online</span>
     </div>
   );
-}
+}       
