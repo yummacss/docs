ad3fc6171f97e9492dcc8ccf23253767b448cc51
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-20 13:51:36 +0100

    fix: update badge components to use "Label" instead of "Online"

--- src/registry/ui/badge-soft-pilled.tsx
+++ src/registry/ui/badge-soft-pilled.tsx
@@ -3,7 +3,7 @@
 export default function BadgeSoftPilled() {
   return (
     <div className="d-f ai-c g-1 px-2 py-1 bg-indigo/10 bc-indigo-2/80 br-pill bw-1">
-      <span className="c-indigo-6 fs-xs fw-500 us-none">Online</span>
+      <span className="c-indigo-6 fs-xs fw-500 us-none">Label</span>
     </div>
   );
 }
