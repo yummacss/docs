05a2879e9e0d269cba28dc708576100302f3c7eb
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-23 22:16:05 +0100

    feat: update BadgeDotIconRightPill component to use LockOpen icon and change labels to "Public"

--- src/registry/ui/badge-dot-icon-right-pill.tsx
+++ src/registry/ui/badge-dot-icon-right-pill.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import { ArrowRight, CircleCheck } from "@gravity-ui/icons";
+import { LockOpen } from "@gravity-ui/icons";
 
 export default function BadgeDotIconRightPill() {
   return (
     <div className="d-f fd-c g-6 ai-c">
       <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 br-pill bw-1 bs-o-xs">
-        <span className="c-slate-10 fs-xs fw-500 us-none">Done</span>
-        <CircleCheck className="w-3 h-3 c-green-6" />
+        <span className="c-slate-10 fs-xs fw-500 us-none">Public</span>
+        <LockOpen className="w-3 h-3 c-green-6" />
       </div>
       <div className="d-f ai-c g-1 px-2 py-1 bg-indigo-1 bc-indigo-2 br-pill bw-1">
-        <span className="c-indigo-7 fs-xs fw-500 us-none">New</span>
-        <ArrowRight className="w-3 h-3 c-indigo-7" />
+        <span className="c-indigo-7 fs-xs fw-500 us-none">Public</span>
+        <LockOpen className="w-3 h-3 c-indigo-7" />
       </div>
       <div className="d-f ai-c g-1 px-2 py-1 bg-indigo c-white br-pill bw-0">
-        <span className="fs-xs fw-500 us-none">Go</span>
-        <ArrowRight className="w-3 h-3" />
+        <span className="fs-xs fw-500 us-none">Public</span>
+        <LockOpen className="w-3 h-3" />
       </div>
     </div>
   );
