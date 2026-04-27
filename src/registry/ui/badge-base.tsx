22851fa18925d4aa13408f0ffeb3f6da1ee2eaf0
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-27 22:28:44 +0100

    feat(button): add ButtonPill component with multiple button styles

--- src/registry/ui/badge-base.tsx
+++ src/registry/ui/badge-base.tsx
@@ -6,7 +6,7 @@ export default function BadgeBase() {
       <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 br-sm bw-1 bs-o-xs">
         <span className="c-slate-10 fs-xs fw-500 us-none">Draft</span>
       </div>
-      <div className="d-f ai-c g-1 px-2 py-1 bg-slate-1 bc-slate-2 br-sm bw-1">
+      <div className="d-f ai-c g-1 px-2 py-1 bg-slate-1 br-sm">
         <span className="c-slate-7 fs-xs fw-500 us-none">Draft</span>
       </div>
       <div className="d-f ai-c g-1 px-2 py-1 bg-slate c-white br-sm bw-0">
