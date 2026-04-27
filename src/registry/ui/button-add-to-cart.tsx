c68ed31de83ea8027822a6523efe5377da838666
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-04-27 21:59:30 +0100

    fix(button): update background color for 'Add to Cart' button and hover state

--- src/registry/ui/button-add-to-cart.tsx
+++ src/registry/ui/button-add-to-cart.tsx
@@ -5,7 +5,7 @@ import { ShoppingCart } from "@gravity-ui/icons";
 
 export default function ButtonAddToCart() {
   return (
-    <Button className="d-if ai-c g-2 px-3 py-2 bg-white bc-silver-2 c-slate-10 br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-6">
+    <Button className="d-if ai-c g-2 px-3 py-2 bg-silver-1/30 bc-silver-2 c-slate-10 br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/80 fv:ow-2 fv:oo-2 fv:oc-indigo-6">
       <ShoppingCart className="w-4 h-4" />
       Add to Cart
     </Button>
