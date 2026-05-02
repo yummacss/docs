5f6aeba0437a8d72418f78101f8102d690f307a6
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-02 20:19:35 +0100

    fix: reorder imports and adjust class names for improved readability in FieldPassword component

--- src/registry/ui/field-password.tsx
+++ src/registry/ui/field-password.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { Field } from "@base-ui/react/field";
 import { Button } from "@base-ui/react/button";
+import { Field } from "@base-ui/react/field";
 import { Eye, EyeSlash } from "@gravity-ui/icons";
 import { useState } from "react";
 
@@ -13,14 +13,14 @@ export default function FieldPassword() {
       <Field.Label className="fw-500">
         Password <span className="c-red-5">*</span>
       </Field.Label>
-      <div className="d-f ai-c p-r">
+      <div className="d-f p-r ai-c">
         <Field.Control
           type={visible ? "text" : "password"}
           placeholder="Enter password"
           aria-label="Enter password"
           className="h-10 w-64 pl-4 pr-10 bg-white bc-silver-3 c-slate-10 bw-1 br-lg fs-md bs-o-xs fv:oo--1 fv:oc-indigo-6"
         />
-        <div className="d-f ai-c jc-c p-a r-3 c-slate-6">
+        <div className="d-f p-a r-3 ai-c jc-c c-slate-6">
           <Button
             className="d-f ai-c jc-c p-0 c-slate-6 c-p us-none"
             aria-label={visible ? "Hide password" : "Show password"}
