b0670290d0f52afd57be00b92f3b04dfe731dce5
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-09 19:55:42 +0100

    fix: update button text from "Create project" to "Create team"

--- src/registry/ui/button-icon.tsx
+++ src/registry/ui/button-icon.tsx
@@ -5,7 +5,7 @@ export default function ButtonIcon() {
   return (
     <Button className="d-if ai-c g-2 px-3 py-2 bg-indigo h:bg-indigo-8 bc-indigo-7 c-white br-md bw-1 fw-500 bs-o-md tp-c tdu-150 ttf-io us-none c-p fv:ow-2 fv:oo-2 fv:oc-indigo-5">
       <Persons className="w-4 h-4" />
-      Create project
+      Create team
     </Button>
   );
 }
