8b52c1ed205cab290d30122ba1a243bfc328b5c3
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-09 21:32:12 +0100

    feat: rename empty state component to EmptyStateText and update registry

--- src/registry/ui/empty-state-text.tsx
+++ src/registry/ui/empty-state-text.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export default function EmptyStateSimple() {
+export default function EmptyStateText() {
   return (
     <div className="d-f fd-c ai-c jc-c g-1 p-8">
       <span className="c-slate-10 fs-md fw-500">No notifications</span>
