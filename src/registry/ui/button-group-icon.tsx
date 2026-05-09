f10e5bf7df864cd1e0f1ee9e2983bac90b9ad54b
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-10 00:01:53 +0100

    fix: replace icon components in ButtonGroupIcon with text formatting icons

--- src/registry/ui/button-group-icon.tsx
+++ src/registry/ui/button-group-icon.tsx
@@ -1,23 +1,19 @@
 "use client";
 
 import { Button } from "@base-ui/react/button";
-import {
-  ArrowDownToLine,
-  ArrowsExpandHorizontal,
-  FolderPlus,
-} from "@gravity-ui/icons";
+import { Bold, Italic, Underline } from "@gravity-ui/icons";
 
 export default function ButtonGroupIcon() {
   return (
     <div className="d-f">
       <Button className="d-if ai-c jc-c w-10 h-10 bg-white bc-silver-2 c-slate-10 btlr-md bblr-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
-        <FolderPlus className="w-4 h-4" />
+        <Bold className="w-4 h-4" />
       </Button>
       <Button className="d-if ai-c jc-c w-10 h-10 bg-white bc-silver-2 c-slate-10 bxw-0 bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
-        <ArrowDownToLine className="w-4 h-4" />
+        <Italic className="w-4 h-4" />
       </Button>
       <Button className="d-if ai-c jc-c w-10 h-10 bg-white bc-silver-2 c-slate-10 btrr-md bbrr-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
-        <ArrowsExpandHorizontal className="w-4 h-4" />
+        <Underline className="w-4 h-4" />
       </Button>
     </div>
   );
