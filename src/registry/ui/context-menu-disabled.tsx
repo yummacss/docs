4428c7752a936e7df62a1435a3b54e03f2330124
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-10 16:03:16 +0100

    feat: add Disabled state functionality for context menu and menu components

---
+++ src/registry/ui/context-menu-disabled.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ContextMenu } from "@base-ui/react/context-menu";
+
+export default function ContextMenuDisabled() {
+  return (
+    <ContextMenu.Root disabled>
+      <ContextMenu.Trigger className="d-f ai-c jc-c h-48 w-60 bc-silver-3 c-slate-4 bw-1 br-lg fs-sm fw-500 o-50 us-none">
+        Permissions
+      </ContextMenu.Trigger>
+    </ContextMenu.Root>
+  );
+}
---
+++ src/registry/ui/menu-disabled.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Menu } from "@base-ui/react/menu";
+import { Lock } from "@gravity-ui/icons";
+
+export default function MenuDisabled() {
+  return (
+    <Menu.Root disabled>
+      <Menu.Trigger className="d-f b-0 ai-c g-2 px-3 py-2 h-fc bg-white bc-silver-2 c-slate-4 br-lg bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none o-50">
+        Roles <Lock className="fs-0 w-4 h-4" />
+      </Menu.Trigger>
+    </Menu.Root>
+  );
+}
