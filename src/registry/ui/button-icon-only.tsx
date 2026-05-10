740fe7c48042b5535f5d893627c9ad1b763c34c4
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-10 19:30:00 +0100

    feat: restructure ButtonIconOnly component layout and update button styles

--- src/registry/ui/button-icon-only.tsx
+++ src/registry/ui/button-icon-only.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { Button } from "@base-ui/react/button";
 import { Paperclip } from "@gravity-ui/icons";
 
 export default function ButtonIconOnly() {
   return (
-    <Button className="d-if ai-c jc-c w-10 h-10 bg-indigo h:bg-indigo-8 bc-indigo-7 c-white br-md bw-1 bs-o-md tp-c tdu-150 ttf-io us-none c-p fv:ow-2 fv:oo-2 fv:oc-indigo-5">
-      <Paperclip className="w-5 h-5" />
-    </Button>
+    <div className="d-f fd-c g-6 ai-c">
+      <Button className="d-if ai-c jc-c g-2 w-10 h-10 bg-indigo h:bg-indigo-8 h:bg-indigo-1/50 bc-indigo-7 c-white br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Paperclip className="w-4 h-4" />
+      </Button>
+      <Button className="d-if ai-c jc-c g-2 w-10 h-10 bg-white bc-silver-2 c-slate-10 br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Paperclip className="w-4 h-4" />
+      </Button>
+      <Button className="d-if ai-c jc-c g-2 w-10 h-10 bg-silver-1 c-slate-7 br-md fw-500 tp-c tdu-150 ttf-io us-none h:bg-silver-2 h:bg-red-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Paperclip className="w-4 h-4" />
+      </Button>
+    </div>
   );
 }
