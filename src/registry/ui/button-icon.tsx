3c0f548903def3b2ad6f6fc0bb32a2ad95729d0d
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-11 20:23:34 +0100

    ui(docs): refactor ButtonIcon component structure and update icon import

--- src/registry/ui/button-icon.tsx
+++ src/registry/ui/button-icon.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import { Button } from "@base-ui/react/button";
-import { Persons } from "@gravity-ui/icons";
+import { Person } from "@gravity-ui/icons";
 
 export default function ButtonIcon() {
   return (
-    <Button className="d-if ai-c g-2 px-3 py-2 bg-indigo h:bg-indigo-8 bc-indigo-7 c-white br-md bw-1 fw-500 bs-o-md tp-c tdu-150 ttf-io us-none c-p fv:ow-2 fv:oo-2 fv:oc-indigo-5">
-      <Persons className="w-4 h-4" />
-      Create team
-    </Button>
+    <div className="d-f fd-c g-6 ai-c">
+      <Button className="d-if ai-c g-2 px-3 py-2 bg-indigo h:bg-indigo-8 h:bg-indigo-1/50 bc-indigo-7 c-white br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Person className="w-4 h-4" />
+        Create team
+      </Button>
+      <Button className="d-if ai-c g-2 px-3 py-2 bg-white bc-silver-2 c-slate-10 br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Person className="w-4 h-4" />
+        Create team
+      </Button>
+      <Button className="d-if ai-c g-2 px-3 py-2 bg-silver-1 c-slate-7 br-md fw-500 tp-c tdu-150 ttf-io us-none h:bg-silver-2 h:bg-red-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Person className="w-4 h-4" />
+        Create team
+      </Button>
+    </div>
   );
 }
