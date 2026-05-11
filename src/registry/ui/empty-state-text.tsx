b17cc4ee0449595b2bdce538cbb99b159ae1d46e
Renildo Pereira <56491937+rrenildopereiraa@users.noreply.github.com>
2026-05-11 17:30:21 +0100

    ui(docs: empty state): add ta-c utility

--- src/registry/ui/empty-state-text.tsx
+++ src/registry/ui/empty-state-text.tsx
@@ -4,7 +4,7 @@ export default function EmptyStateText() {
   return (
     <div className="d-f fd-c ai-c jc-c g-1 p-8">
       <span className="c-slate-10 fs-md fw-500">No notifications</span>
-      <span className="c-slate-6 fs-sm">
+      <span className="c-slate-6 ta-c fs-sm">
         You're all caught up! New updates will appear here.
       </span>
     </div>
