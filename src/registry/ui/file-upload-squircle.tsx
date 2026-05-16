9097ab89e40b28bb13c3763893c53d521cd7cf0e
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-16 18:21:08 +0100

    feat: add new File Upload variants for square and squircle shapes; update documentation and registry

---
+++ src/registry/ui/file-upload-squircle.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { CloudArrowUpIn } from "@gravity-ui/icons";
+
+export default function FileUploadSquircle() {
+  return (
+    <section className="d-f fd-c ai-c g-3 w-100 bc-silver-2 br-3xl cs-s bw-2 bs-d">
+      <div className="d-f fd-c ai-c g-2 p-8 bg-white ta-c">
+        <div className="d-f ai-c jc-c w-10 h-10 bg-white bc-silver-2 br-xxl cs-s bw-1 bs-o-xs">
+          <CloudArrowUpIn className="w-5 h-5 c-slate-6" />
+        </div>
+        <div className="d-f fd-c ai-c g-1">
+          <span className="c-slate-10 fs-sm fw-500">
+            <a className="c-indigo td-none c-p">Upload project assets</a> or
+            drag and drop
+          </span>
+          <span className="c-slate-6 fs-xs fw-400">
+            Drag and drop project files here
+          </span>
+        </div>
+      </div>
+    </section>
+  );
+}
