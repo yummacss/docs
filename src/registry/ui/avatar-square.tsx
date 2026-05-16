4039ea97437cb941b9c5756b4f226173244e675a
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-16 22:17:12 +0100

    feat: add AvatarSquare and AvatarSquircle components with corresponding registry entries

---
+++ src/registry/ui/avatar-square.tsx
@@ -0,0 +1,19 @@
+import { Avatar } from "@base-ui/react/avatar";
+
+export default function AvatarSquare() {
+  return (
+    <Avatar.Root className="d-if o-h ai-c jc-c w-12 h-12 bg-silver-1 bc-white bw-1 va-m us-none">
+      <Avatar.Image
+        src={userProfile.avatarUrl}
+        alt={userProfile.name}
+        className="of-c w-100% h-100%"
+      />
+    </Avatar.Root>
+  );
+}
+
+const userProfile = {
+  name: "Sarah",
+  avatarUrl:
+    "https://api.dicebear.com/9.x/open-peeps/svg?seed=Sarah&backgroundColor=DAF0B9",
+};
