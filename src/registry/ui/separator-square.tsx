26ba03ca4b4b2588471123d92e8f7680594fc927
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-16 23:17:25 +0100

    feat: add SeparatorSquare and SeparatorSquircle components with corresponding registry entries and update documentation

---
+++ src/registry/ui/separator-square.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@base-ui/react/button";
+import { Separator } from "@base-ui/react/separator";
+import { Plus } from "@gravity-ui/icons";
+
+export default function SeparatorSquare() {
+  return (
+    <div className="d-f ai-c g-2 w-100%">
+      <Separator className="fg-1 h-px bg-silver-2" />
+      <Button className="d-if ai-c jc-c w-8 h-8 bg-white bc-silver-2 c-slate-10 bw-1 bs-o-xs tp-c tdu-150 ttf-io us-none c-p h:bg-silver-1/50 fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+        <Plus className="w-5 h-5" />
+      </Button>
+      <Separator className="fg-1 h-px bg-silver-2" />
+    </div>
+  );
+}
