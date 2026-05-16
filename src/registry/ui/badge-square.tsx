76b284d8b4fd90c223f969c2a525ca6ddcdaf8d6
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-16 22:34:22 +0100

    feat: add BadgeSquare and BadgeSquircle components with registry entries

---
+++ src/registry/ui/badge-square.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BadgeSquare() {
+  return (
+    <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 bw-1 bs-o-xs">
+      <span className="c-slate-10 fs-xs fw-500 us-none">Label</span>
+    </div>
+  );
+}
---
+++ src/registry/ui/badge-squircle.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BadgeSquircle() {
+  return (
+    <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 br-xxl cs-s bw-1 bs-o-xs">
+      <span className="c-slate-10 fs-xs fw-500 us-none">Label</span>
+    </div>
+  );
+}
