491deebc46cd5d310509b74cba2a74ba7f35b0ca
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-16 23:49:06 +0100

    feat: add ButtonIconLeading, ButtonIconTrailing, ButtonSquare, and ButtonSquircle components with corresponding registry entries

---
+++ src/registry/ui/button-icon-leading.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@base-ui/react/button";
+import { Person } from "@gravity-ui/icons";
+
+export default function ButtonIconLeading() {
+  return (
+    <Button className="d-if ai-c g-2 px-3 py-2 bg-indigo h:bg-indigo-8 bc-indigo-7 c-white br-md bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+      <Person className="w-4 h-4" />
+      Add member
+    </Button>
+  );
+}
---
+++ src/registry/ui/button-square.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@base-ui/react/button";
+
+export default function ButtonSquare() {
+  return (
+    <Button className="d-if ai-c px-3 py-2 bg-indigo h:bg-indigo-8 bc-indigo-7 c-white bw-1 fw-500 bs-o-xs tp-c tdu-150 ttf-io us-none fv:ow-2 fv:oo-2 fv:oc-indigo-5">
+      Add member
+    </Button>
+  );
+}
