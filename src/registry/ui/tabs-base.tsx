998941ff4791b3da7b68a8ff2bae11e5cfbcb28f
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-17 15:19:20 +0100

    feat: implement ExampleTabs component with tab navigation and icons

--- src/registry/ui/tabs-base.tsx
+++ src/registry/ui/tabs-base.tsx
@@ -1,5 +1,50 @@
 "use client";
 
-export default function TabsBase() {
-  return <p className="c-red fw-500">Coming Soon...</p>;
+import { Tabs } from "@base-ui/react/tabs";
+import { Clock, ListUl } from "@gravity-ui/icons";
+import { useState } from "react";
+
+export default function ExampleTabs() {
+  const [selected, setSelected] = useState("sprint");
+
+  return (
+    <Tabs.Root
+      value={selected}
+      onValueChange={setSelected}
+      className="w-fc bg-silver-1/50 br-9999 bw-1"
+    >
+      <Tabs.List className="d-f p-r g-1 p-1">
+        {tabs.map((tab) => {
+          const Icon = tab.icon;
+          const isSelected = selected === tab.value;
+          return (
+            <Tabs.Tab
+              key={tab.value}
+              value={tab.value}
+              className={`p-r zi-10 fg-1 d-f ai-c jc-c g-2 py-2 px-3 bw-0 bg-transparent c-p us-none fv:oo--1 fv:oc-indigo-5 ${
+                isSelected ? "c-slate-10" : "c-slate-8 h:c-slate-10"
+              }`}
+            >
+              <Icon className="p-r zi-10 w-5 h-5" />
+              <span className="p-r zi-10 fs-sm fw-500">{tab.label}</span>
+            </Tabs.Tab>
+          );
+        })}
+        <Tabs.Indicator
+          className="p-a l-0 zi-0 bg-white br-9999 bs-o-xs tp-a tdu-200 ttf-io"
+          style={{
+            translate: "var(--active-tab-left) 0",
+            width: "var(--active-tab-width)",
+            top: "var(--active-tab-top)",
+            height: "var(--active-tab-height)",
+          }}
+        />
+      </Tabs.List>
+    </Tabs.Root>
+  );
 }
+
+const tabs = [
+  { value: "sprint", label: "Sprint", icon: Clock },
+  { value: "backlog", label: "Backlog", icon: ListUl },
+];
