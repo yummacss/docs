11e89aefee18b6c0f12a7eed1066e414191f03b2
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-28 02:02:13 +0100

    remove "fill="currentColor"

--- src/registry/ui/badge-icon-pill.tsx
+++ src/registry/ui/badge-icon-pill.tsx
@@ -6,15 +6,15 @@ export default function BadgeIconPill() {
   return (
     <div className="d-f fd-c g-6 ai-c">
       <div className="d-f ai-c g-1 px-2 py-1 bg-white bc-silver-2 br-9999 bw-1 bs-o-xs">
-        <Bookmark fill="currentColor" className="w-3 h-3 c-slate-10" />
+        <Bookmark className="w-3 h-3 c-slate-10" />
         <span className="c-slate-10 fs-xs fw-500 us-none">Saved</span>
       </div>
       <div className="d-f ai-c g-1 px-2 py-1 bg-indigo-1 br-9999 bw-0">
-        <Bookmark fill="currentColor" className="w-3 h-3 c-indigo-7" />
+        <Bookmark className="w-3 h-3 c-indigo-7" />
         <span className="c-indigo-7 fs-xs fw-500 us-none">Saved</span>
       </div>
       <div className="d-f ai-c g-1 px-2 py-1 bg-indigo c-white br-9999 bw-0">
-        <Bookmark fill="currentColor" className="w-3 h-3" />
+        <Bookmark className="w-3 h-3" />
         <span className="fs-xs fw-500 us-none">Saved</span>
       </div>
     </div>
--- src/registry/ui/button-favorite.tsx
+++ src/registry/ui/button-favorite.tsx
@@ -6,7 +6,7 @@ import { Star } from "iconoir-react";
 export default function ButtonStar() {
   return (
     <Button className="d-if ai-c g-2 px-3 py-2 bg-transparent c-slate-10 br-md bw-0 fw-500 tp-c tdu-150 ttf-io us-none fv:oo-2 fv:oc-indigo-3">
-      <Star fill="currentColor" className="w-4 h-4 c-yellow-5" />
+      <Star className="w-4 h-4 c-yellow-5" />
       24
     </Button>
   );
