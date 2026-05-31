43a2c3c3a22fd1e546ac2f5388fe3a9589078d4c
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-05-31 12:06:21 +0100

    use StarSolid instead of Star (outlined icon)

--- src/registry/ui/button-favorite.tsx
+++ src/registry/ui/button-favorite.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Button } from "@base-ui/react/button";
-import { Star } from "iconoir-react";
+import { StarSolid } from "iconoir-react";
 
 export default function ButtonStar() {
   return (
     <Button className="d-if ai-c g-2 px-3 py-2 bg-transparent c-slate-10 br-md bw-0 fw-500 tp-c tdu-150 ttf-io us-none fv:oo-2 fv:oc-indigo-3">
-      <Star className="w-4 h-4 c-yellow-5" />
+      <StarSolid className="w-4 h-4 c-yellow-5" />
       24
     </Button>
   );
