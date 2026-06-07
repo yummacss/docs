f500588671f2b796b108537e2d7c94553d163bb4
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-06-08 00:12:16 +0100

    refactor(ui): remove unused button and tab variants, clean up styles

--- src/registry/ui/button-outlined.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@base-ui/react/button";
-
-export default function ButtonOutlined() {
-  return (
-    <Button className="d-if ai-c px-3 py-2 bg-transparent bc-indigo c-indigo br-md bw-1 fw-500 tp-c tdu-150 ttf-io us-none c-p h:bg-indigo-1 fv:oo-2 fv:oc-indigo-3">
-      Track
-    </Button>
-  );
-}
