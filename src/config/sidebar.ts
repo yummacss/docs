e6a87efdc3f4d6bc957d5f9147ed55caceab330a
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-08-17 00:12:47 +0100

    chore: rename Packges label cat to Utils in the sidebar.ts file

--- src/config/sidebar.ts
+++ src/config/sidebar.ts
@@ -357,7 +357,7 @@ export const sidebarConfig = {
     // nobody needs in order to use Yumma CSS, & sitting near Get Started they
     // read as a third setup step.
     {
-      title: "Packages",
+      title: "Utils",
       items: ["intellisense", "canon"],
     },
   ] satisfies SidebarSection[],
