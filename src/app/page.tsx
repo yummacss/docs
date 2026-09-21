c361ef0001de2d35974b447ca016223e2189e4d2
rrenildopereiraa <rrenildopereiraa@gmail.com>
2026-09-21 01:43:20 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
           <div className="d:g g:12 ai:fe @lg:gtc:2">
             <div>
               <Link
-                href="/ui/installation"
+                href="/blog/yummacss-4.0.0"
                 className="d:if ai:c g:2 px:3 py:1 mb:6 bc:border bg:surface c:white/70 bw:1 fs:sm us:none h:c:white fv:oc:white fv:ow:2"
               >
                 Yumma CSS 4.0 is live!
